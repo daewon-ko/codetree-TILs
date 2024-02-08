919430e1e8f1414e05fe5602839ae2a8f3412b0b
daewon-ko <105340285+daewon-ko@users.noreply.github.com>
2024-02-08 18:57:43 +0900

    Title: 합과 평균, Time: 113ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240208/합과 평균/sum-and-avg.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a+b+" "+(double)(a+b)/2);
+
+
+
+    }
+}
