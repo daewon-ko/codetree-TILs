c8f2fa1266a27992f73b3cedfb9f50932f7887df
daewon-ko <105340285+daewon-ko@users.noreply.github.com>
2024-02-14 22:12:35 +0900

    Title: 방향에 맞춰 이동, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240214/방향에 맞춰 이동/move-in-direction.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int [] dx = new int[]{1,0,-1,0};
+    static int [] dy = new int[]{0,-1,0,1};
+
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int x=0;
+        int y =0;
+
+        for(int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String dir = st.nextToken();
+            int depth = Integer.parseInt(st.nextToken());
+
+            for(int j=0; j<depth; j++){
+                if(dir.equals("E")){
+                    x+=dx[0];
+                    y+=dy[0];
+                }else if(dir.equals("S")){
+                    x+=dx[1];
+                    y+=dy[1];
+                }else if(dir.equals("W")){
+                    x+=dx[2];
+                    y+=dx[2];
+                }else if(dir.equals("N")){
+                    x+=dx[3];
+                    y+=dy[3];
+                }
+            }
+        }
+        System.out.print(x+" "+y);
+    }
+}
