64a3b10bdd8c4fc842a7d3e6f71e9e1d3fcc4211
daewon-ko <105340285+daewon-ko@users.noreply.github.com>
2024-02-20 20:44:53 +0900

    Title: 20대의 평균나이, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240220/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = 20;
+        int cnt = 0;
+        double sum = 0;
+        while(n>=20 && n<30){
+            n = sc.nextInt();
+            if(n>=30){
+                break;
+            }
+            sum+=n;
+            cnt++;
+        }
+        System.out.printf("%.2f", (double)sum/cnt);
+    }
+}
