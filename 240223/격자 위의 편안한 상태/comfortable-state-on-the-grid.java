50c2d9920ccd272d7fa0d3de84ec38eda8d7d24d
daewon-ko <105340285+daewon-ko@users.noreply.github.com>
2024-02-23 09:46:30 +0900

    Title: 격자 위의 편안한 상태, Time: 279ms, Memory: 13MB, Status: Passed - CodeTree

--- 240223/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
+++ 240223/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
@@ -1,6 +1,12 @@
 import java.util.*;
 import java.io.*;
 public class Main {
+    /**
+    * 배열 인덱스로 골치썩음
+
+    어떻게 하는게 가장 좋은 방법일까? 
+    노상 헷갈리는 개념이다
+    */
     static int [][] graph;
     static int [] dx = new int[]{0,1,0,-1};
     static int [] dy = new int[]{1,0,-1,0};
@@ -13,20 +19,20 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        graph = new int[n][n];
+        graph = new int[n+1][n+1];
         
 
         for(int i=0; i<m; i++){
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            graph[x-1][y-1] = 1;
+            graph[x][y] = 1;
             int cnt = 0;
 
             for(int dirNum=0; dirNum<4; dirNum++){
                 
-                int nx = x-1+dx[dirNum];
-                int ny = y-1+dy[dirNum];
+                int nx = x+dx[dirNum];
+                int ny = y+dy[dirNum];
 
                 if(inRange(nx,ny)&& graph[nx][ny]==1){
                     cnt++;
@@ -43,6 +49,6 @@ public class Main {
 
     }
     public static boolean inRange(int x, int y){
-        return x>=0 && x<n && y>=0 && y<n;
+        return x>=1 && x<n+1 && y>=1 && y<n+1;
     }
 }
