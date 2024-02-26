4bc55bd5ec45b1952e5b7583f6d89dcfc3e21f5d
daewon-ko <105340285+daewon-ko@users.noreply.github.com>
2024-02-26 11:42:07 +0900

    Title: 사칙연산 함수, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240226/사칙연산 함수/quadratic-operations-function.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        String o = st.nextToken();
+        int c = Integer.parseInt(st.nextToken());
+        calc(a,c,o);
+
+
+    }
+    public static void calc(int a, int c, String o){
+        if(o.equals("+")){
+            System.out.println(a+" + "+ c +" = "+(a+c));
+        }else if(o.equals("-")){
+            System.out.println(a+" - "+ c +" = "+(a-c));
+        }else if(o.equals("/")){
+            System.out.println(a+" / "+ c +" = "+(int)(a/c));
+        }else{
+            System.out.println(a+" * "+ c +" = "+(a*c));
+        }
+    }
+}
