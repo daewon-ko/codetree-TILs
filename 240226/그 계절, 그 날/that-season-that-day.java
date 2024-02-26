45cb93310f794c5a1b94925cf081bae46863ee1e
daewon-ko <105340285+daewon-ko@users.noreply.github.com>
2024-02-26 19:19:41 +0900

    Title: 그 계절, 그 날, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240226/그 계절, 그 날/that-season-that-day.java
+++ 240226/그 계절, 그 날/that-season-that-day.java
@@ -63,8 +63,8 @@ public class Main {
             return true;
         }
 
-        if(!(y%4==0 && y%100==0)){
-            return true;
+        if(y%4==0 && y%100==0){
+            return false;
         }
         if(y%4==0 && y%100==0 && y%400==0){
             return true;
