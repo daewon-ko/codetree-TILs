8bd284c4537c7fd71a580c58f51ea65b13f4ec40
daewon-ko <105340285+daewon-ko@users.noreply.github.com>
2024-02-28 11:06:45 +0900

    Title: 두 정수에 대한 연산값 2, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240228/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
+++ 240228/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.java
@@ -1,75 +1,21 @@
-// import java.util.*;
-// public class Main {
-
-//     public static void main(String[] args) {
-//         // 여기에 코드를 작성해주세요.
-//         Scanner sc = new Scanner(System.in);
-
-//         int a = sc.nextInt();
-//         int b = sc.nextInt();
-
-
-//         int min = Math.min(a,b);
-//         int max = Math.max(a,b);
-
-//         min+=10;
-//         max*=2;
-
-
-
-//         System.out.print(min+" "+max);
-
-//     }
-//     public static int getMin(int a, int b){
-//         int min = 0;
-
-//         if(a<b){
-//             min = a;
-//         }else{
-//             min = b;
-//         }
-//         return min;
-//     }
-//     public static int getMax(int a, int b){
-//         int max = 0;
-//         if(a<b){
-//             max = b;
-//             return max;
-//         }
-//         return a;
-//     }
-// }
-
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-
+    static int a,b;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-
-        // 함수 호출을 통해 값을 수정
-         a = getMin(a, b) + 10;
-        b = getMax(a, b) * 2;
-
-        // 함수 호출 이후에 값을 출력
+        int i = sc.nextInt();
+        int j = sc.nextInt();
+        func(i,j);
         System.out.print(a + " " + b);
     }
-
-    public static int getMin(int a, int b) {
-        if (a < b) {
-            return a;
-        } else {
-            return b;
+    static void func(int i, int j){
+        if(i>j){
+            b = j+ 10;
+            a = i * 2;
+        }else{
+            a = i + 10;
+            b = j * 2;
         }
-    }
-
-    public static int getMax(int a, int b) {
-        if (a < b) {
-            return b;
-        }
-        return a;
     }
 }
