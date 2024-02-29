c1ba83288f0fbf6a7a4bf6fb53e53ea2f76d1f4b
daewon-ko <105340285+daewon-ko@users.noreply.github.com>
2024-02-29 19:59:19 +0900

    Title: 재귀함수의 꽃, Time: 163ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240229/재귀함수의 꽃/the-flower-of-the-recursive-function.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        printNum(n);
+
+
+    }
+    public static void printNum(int n){
+
+        if(n==0){
+            return;
+        }
+
+        System.out.print(n+" ");
+
+        printNum(n-1);
+   
+        System.out.print(n+" ");
+
+    }
+}
