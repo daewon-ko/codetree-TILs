3ff6ed52f8d970a143f5623e980b9914f04b3c03
daewon-ko <105340285+daewon-ko@users.noreply.github.com>
2024-02-29 14:39:58 +0900

    Title: 반복 출력하기 2, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240229/반복 출력하기 2/repeated-output-2.java
+++ 240229/반복 출력하기 2/repeated-output-2.java
@@ -5,6 +5,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        func(n);
     }
     public static void func(int n){
         if(n==0){
