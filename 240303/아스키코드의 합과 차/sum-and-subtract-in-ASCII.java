b562453c017a00e230b0da6ea2d4452d8f62d4c0
daewon-ko <105340285+daewon-ko@users.noreply.github.com>
2024-03-03 15:46:11 +0900

    Title: 아스키코드의 합과 차, Time: 118ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240303/아스키코드의 합과 차/sum-and-subtract-in-ASCII.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        String word1 = sc.next();
+        String word2 = sc.next();
+        int word1_num = (int)word1.charAt(0);
+        int word2_num = (int)word2.charAt(0);
+        if(word1_num> word2_num){
+            System.out.println(word1_num+word1_num+" "+(word1_num-word2_num));
+        }else{
+            System.out.println(word1_num+word2_num+" "+(word2_num-word1_num));
+        }
+
+
+    }
+}
