2402c090b0394f936a1a174fdec467b6348ca302
daewon-ko <105340285+daewon-ko@users.noreply.github.com>
2024-03-05 16:07:30 +0900

    Title: 원 모양으로 되어있는 방, Time: 231ms, Memory: 13MB, Status: Passed - CodeTree

--- 240305/원 모양으로 되어있는 방/a-room-in-a-circle.java
+++ 240305/원 모양으로 되어있는 방/a-room-in-a-circle.java
@@ -4,16 +4,18 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
         int [] arr = new int[n+1];
+
         for(int i=1; i<=n; i++){
             arr[i] = sc.nextInt();
         }
 
-
-
         for(int i=1; i<=n; i++){    // i번쨰가 기준방
             int sum = 0;
+
+
 
             for(int j=(i+1); j<=n; j++){ // n-i번만큼 돌 수 있음
 
@@ -28,8 +30,12 @@ public class Main {
             }
 
             for(int j=1; j<i; j++){
-                sum+=((5-i)+j)*arr[j];
+                if(i==j){
+                    continue;
+                }
+                sum+=((n-i)+j)*arr[j];
             }
+
             min = Math.min(min,sum);
         }
 
