31c00f9c6a02623a8119b338721eda6304b28078
daewon-ko <105340285+daewon-ko@users.noreply.github.com>
2024-03-21 16:45:40 +0900

    Title: 삼각형 만들기, Time: 96ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240321/삼각형 만들기/create-triangle.java
+++ 240321/삼각형 만들기/create-triangle.java
@@ -38,7 +38,7 @@ public class Main {
         
                     
                      ){
-                        int result = Math.abs((x1*y2 + x2*y3 + x3*y1) -(x2*y1 + x3*y2 + x1*y3))/2;
+                        int result = (int)Math.abs((x1*y2 + x2*y3 + x3*y1) -(x2*y1 + x3*y2 + x1*y3))/2;
                         max = Math.max(max, result);
                     }
                 }
